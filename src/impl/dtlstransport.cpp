d0082da48a62dca53824d7525123d654cd6ba12c
Gasper Lah <39318629+gasperov@users.noreply.github.com>
2026-09-13 14:11:46 +0200

    Use tryPush for DTLS incoming queue in GnuTLS, MbedTLS and SChannel backends

--- src/impl/dtlstransport.cpp
+++ src/impl/dtlstransport.cpp
@@ -162,8 +162,11 @@ void DtlsTransport::incoming(message_ptr message) {
 	}
 
 	PLOG_VERBOSE << "Incoming size=" << message->size();
-	mIncomingQueue.push(std::move(message));
-	enqueueRecv();
+	if (mIncomingQueue.tryPush(std::move(message))) {
+		enqueueRecv();
+	} else {
+		PLOG_VERBOSE << "DTLS incoming queue is full, dropping";
+	}
 }
 
 bool DtlsTransport::outgoing(message_ptr message) {
@@ -494,8 +497,11 @@ void DtlsTransport::incoming(message_ptr message) {
 	}
 
 	PLOG_VERBOSE << "Incoming size=" << message->size();
-	mIncomingQueue.push(std::move(message));
-	enqueueRecv();
+	if (mIncomingQueue.tryPush(std::move(message))) {
+		enqueueRecv();
+	} else {
+		PLOG_VERBOSE << "DTLS incoming queue is full, dropping";
+	}
 }
 
 bool DtlsTransport::outgoing(message_ptr message) {
@@ -828,8 +834,11 @@ void DtlsTransport::incoming(message_ptr message) {
 	}
 
 	PLOG_VERBOSE << "Incoming size=" << message->size();
-	mIncomingQueue.push(std::move(message));
-	enqueueRecv();
+	if (mIncomingQueue.tryPush(std::move(message))) {
+		enqueueRecv();
+	} else {
+		PLOG_VERBOSE << "DTLS incoming queue is full, dropping";
+	}
 }
 
 bool DtlsTransport::outgoing(message_ptr message) {
@@ -1369,7 +1378,7 @@ void DtlsTransport::incoming(message_ptr message) {
 	}
 
 	PLOG_VERBOSE << "Incoming size=" << message->size();
-	if(mIncomingQueue.tryPush(std::move(message))) {
+	if (mIncomingQueue.tryPush(std::move(message))) {
 		enqueueRecv();
 	} else {
 		PLOG_VERBOSE << "DTLS incoming queue is full, dropping";
